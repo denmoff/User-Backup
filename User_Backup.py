cb798aa9a26042433960751ea81fe67d25949554
denmoff <denmoff@gmail.com>
2015-05-08 14:45:21 -0400

    replaced print statements with logging

--- User_Backup.py
+++ User_Backup.py
@@ -22,7 +22,7 @@ backup_dir = os.path.join(dest_path, comp_name)
 last_run_file = os.path.join(dest_path, "." + comp_name)
 run_interval = 90 #minutes
 #log_path = os.path.join(source_path,'Library', 'Logs','User_Backup.log')
-log_path = os.path.join('/Users','Shared','.User_Backup.log')
+log_path = os.path.join('/Users','Shared','User_Backup.log')
 
 
 ### If rsyc3 exsists, use it. Otherwise, use system rsync.
@@ -45,25 +45,25 @@ logging.debug(user_name)
 def main():
     # x_string = create_exclude_string(exclusion_list)
     if flight_check():
-        print "Running backup..."
+        logging.info("Running backup...")
         rsync_command(source_path,backup_dir) 
 
 
 def flight_check():
     go1 = os.path.isdir(backup_dir) # sets go1 to true if backup directory exists.
     if go1:
-        print "The Destination is: %s and it appears to be available. The Computername is: %s" % (dest_path,comp_name)
+        logging.info("The Destination is: %s and it appears to be available. The Computername is: %s" % (dest_path,comp_name))
     else:
-        print "The Destination is: %s and it DOES NOT appear to be available. The Computername is: %s" % (dest_path,comp_name)
-        print "Attempting to create backup directory..."
+        logging.info("The Destination is: %s and it DOES NOT appear to be available. The Computername is: %s" % (dest_path,comp_name))
+        logging.info("Attempting to create backup directory...")
         try:
             os.makedirs(backup_dir)
             if os.path.isdir(backup_dir):
-                print "Backup directory successfully created."
+                logging.info("Backup directory successfully created.")
                 go1 = True
         except Exception:
-            print "Could not create backup directory."
-            print "Exiting."
+            logging.info("Could not create backup directory.")
+            logging.info("Exiting.")
             return False
 
     if os.path.isfile(last_run_file):
@@ -71,25 +71,25 @@ def flight_check():
             go2 = True
         else:
             go2 = False
-            print "Backup has run within the last %s minutes" % run_interval
+            logging.info("Backup has run within the last %s minutes" % run_interval)
     else: # If Last Run File is missing, try to create it. If it fails, set go2 to False. If it succeeds, set to True.
         go2 = False
-        print "Missing the last run file. Will attempt to create it..."
+        logging.info("Missing the last run file. Will attempt to create it...")
         try:
             open(last_run_file, 'a').close()
             if os.path.isfile(last_run_file):
-                print "File successfully created."
+                logging.info("File successfully created.")
                 go2 = True
         except Exception:
-            print "Could not create file."
+            logging.info("Could not create file.")
 
     if commands.getoutput('pgrep rsync') == '':
         go3 = True
     else:
         go3 = False
-        print "An instance of rsync is already running. Will try again shortly."
+        logging.info("An instance of rsync is already running. Will try again shortly.")
 
-    # print "Backup directory exists: %s, a backup has not already run recently: %s, rsync is not currently running: %s" % (go1,go2,go3)
+    # logging.info("Backup directory exists: %s, a backup has not already run recently: %s, rsync is not currently running: %s" % (go1,go2,go3))
     
     if go1 and go2 and go3:
         return True
@@ -109,7 +109,7 @@ def rsync_command(src,dst):
     rp = Popen(rsync_args,stdout=PIPE,stderr=PIPE,shell=False)
     rsync_stdout,rsync_stderr = rp.communicate()
     logging.debug(rsync_stdout)
-    print "Rsync Complete."
+    logging.info("Rsync Complete.")
     os.utime(last_run_file,None)
 
 if __name__ == "__main__":
